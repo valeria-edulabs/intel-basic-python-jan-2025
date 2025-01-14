e27b652f7c31658c2d9fedc57e4cd6b29dc88957
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2025-01-14 11:32:13 +0200

    lesson1

--- meeting1/basics.py
+++ meeting1/basics.py
@@ -1,41 +1,117 @@
-print("Hello world")
-print("Valeria")
-print("Hello intel")
+# import datetime
+#
+# print("Hello world")
+# print("Valeria")
+# print("Hello intel")
+#
+# num = 3
+# print(type(num))
+# num = "sun"
+# print(type(num))
+#
+# num1 = 3.4
+# print(type(num1))
+#
+# print(num + " is blue")
+# print(num, " is blue", 6, print)
+#
+# print(5 + 7.3)
+# # print("www" + 4)
+# print("www" * 10)
+# print(5 // 2)
+# print(5 % 2)
+# print(5 / 2)
+#
+# print(5 < 7)
+# print(True, False)
+#
+# print(5 == 5)
+# print(5 != 5)
+#
+# b1 = 6 < 7
+# b2 = 7 == 8
+#
+# print(b1 and b2)
+# print(b1 or b2)
+#
+# print(not b1)
+#
+# # print = 5
+# # print("Sdfsdf")
+#
+# a = print("hello")
+# print(a)
+#
+# print(print("hello"))
+#
+# print(3 + 4)
 
-num = 3
-print(type(num))
-num = "sun"
-print(type(num))
+# name = input()
+# print(name)
 
-num1 = 3.4
-print(type(num1))
+# name = input("Insert you name: ")
+# print(name)
+#
+# year = input("Insert your birth year: ")
+# print(year, type(year))
+#
+# age = 2025 - int(year)
+# print("age", age)
+#
+# # print('sun')
+# # print('He said: "The life is good"')
+#
+# print("Hi", name, "You are", age, "years old")
+#
+# print(f"Hi {name}, you are {age} years old")
+#
+# print(f"Hi {name}, you are {age:5d} years old")
+#
+# num = 5
+#
+text = "the life is beautiful and the sky is blue yey"
+print(text.upper())
+print(text.lower())
+words = text.split()
+print(words, type(words))
+# print(words * 5)
+print(len(words))
+print(words, type(words), len(words), end="\n", sep="\n")
 
-print(num + " is blue")
-print(num, " is blue", 6, print)
+print(words[0])
+print(words[3])
+# print(words[5])
+print(words[-1])
+print(words[-3])
+print(words.index("life"))
 
-print(5 + 7.3)
-# print("www" + 4)
-print("www" * 10)
-print(5 // 2)
-print(5 % 2)
-print(5 / 2)
+print(words[1:4])
+print(words[4:])
+print(words[:3])
+print(words[2:7:2])
+print(words[::-1])
+print(" ".join(words[::-1]).capitalize())
+# print(str(words[::-1]))
 
-print(5 < 7)
-print(True, False)
+print("Hadar"[3])
 
-print(5 == 5)
-print(5 != 5)
+name = "Hadar"
+print(name[::-1].capitalize())
 
-b1 = 6 < 7
-b2 = 7 == 8
+# num = 12323123
 
-print(b1 and b2)
-print(b1 or b2)
+print(words[3][-1])
 
-print(not b1)
+# 129
+# 02:09
+#
+# 3689
+# 01:01:29
 
+# print()
+#
+#
+#
 
-
-
-# print = 5
-# print("Sdfsdf")
+# year = "1987"
+# year = 1987
