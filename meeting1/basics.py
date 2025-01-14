f76b867c125acda923e29f65d821ce20ad789e9f
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2025-01-14 10:21:21 +0200

    lesson1

--- meeting1/basics.py
+++ meeting1/basics.py
@@ -1,1 +1,41 @@
 print("Hello world")
+print("Valeria")
+print("Hello intel")
+
+num = 3
+print(type(num))
+num = "sun"
+print(type(num))
+
+num1 = 3.4
+print(type(num1))
+
+print(num + " is blue")
+print(num, " is blue", 6, print)
+
+print(5 + 7.3)
+# print("www" + 4)
+print("www" * 10)
+print(5 // 2)
+print(5 % 2)
+print(5 / 2)
+
+print(5 < 7)
+print(True, False)
+
+print(5 == 5)
+print(5 != 5)
+
+b1 = 6 < 7
+b2 = 7 == 8
+
+print(b1 and b2)
+print(b1 or b2)
+
+print(not b1)
+
+
+
+
+# print = 5
+# print("Sdfsdf")
