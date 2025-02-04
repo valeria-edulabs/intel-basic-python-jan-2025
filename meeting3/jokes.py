d9ddbe826c9dfec8d1a8a01ee37ac3322c26bc4c
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2025-02-04 10:11:27 +0200

    lesson2

--- meeting3/jokes.py
+++ meeting3/jokes.py
@@ -1,3 +1,5 @@
+import pprint
+
 import requests
 
 # https://api.chucknorris.io/jokes/random
@@ -5,12 +7,24 @@ import requests
 # https://api.chucknorris.io/jokes/random?category={category}
 # https://api.chucknorris.io/jokes/search?query={query}
 
-response = requests.get("https://api.chucknorris.io/jokes/random")
-print(response.status_code)
-if response.status_code == 200:
-    data = response.text
-    print(type(data))
-    data = response.json()
-    print(data['value'])
+# response = requests.get("https://api.chucknorris.io/jokes/random")
+# print(response.status_code)
+# if response.status_code == 200:
+#     data = response.text
+#     print(type(data))
+#     data = response.json()
+#     print(data['value'])
 # response = requests.get("https://api.chucknorris.io/jokes/randommmm")
 # print(response.status_code)
+
+
+params = {
+    "query": "eat"
+}
+response = requests.get("https://api.chucknorris.io/jokes/search", params)
+print(response.status_code)
+pprint.pprint(response.json())
+data = response.json()
+print('total jokes',len(data))
+for joke in data['result']:
+    print(joke['value'])
