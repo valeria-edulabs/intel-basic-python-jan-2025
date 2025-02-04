a1a1d30b18de20929650dc486035a7143c4c617d
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2025-02-04 10:34:24 +0200

    lesson3

---
+++ meeting3/jokes_with_proxies.py
@@ -0,0 +1,9 @@
+import requests
+
+proxies = {
+    'http': 'http://proxy-chain.intel.com:911',
+    'https': 'http://proxy-chain.intel.com:911',
+}
+
+url = "https://api.chucknorris.io/jokes/random"
+requests.get(url, proxies=proxies)
