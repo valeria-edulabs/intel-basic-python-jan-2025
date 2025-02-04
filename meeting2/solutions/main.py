8e3b91edd54e4f27e5f99a0109ea4f569b5f7bbb
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2025-02-04 12:55:48 +0200

    lesson3

---
+++ meeting2/solutions/main.py
@@ -0,0 +1,26 @@
+import datetime
+
+from meeting2.solutions.count_occurences import count_occurrences
+from meeting2.solutions.israel_id_check_digit import calc_control_digit
+# from meeting2.utils import add_one
+# from meeting2 import utils
+# from meeting2 import add_one, add_two
+# from meeting2.utils import *
+from meeting2.utils import add_one as utils_add_one
+import math
+import random
+import pprint
+
+
+
+def add_one(string):
+    print("sdfsdfsdf")
+
+calc_control_digit("11111111")
+count_occurrences(["a", "b", "c"])
+# add_one(2)
+# utils.add_one(2)
+
+datetime.datetime.now()
+
+
